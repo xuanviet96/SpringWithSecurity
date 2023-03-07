d53045fde8754c7c84835aa6f766f612335dcc15
unknown <nguyenvietxuan96@gmail.com>
2023-03-07 23:41:05 +0700

    remove cascade type all from user to role

--- src/main/java/com/example/springwithsecurity/entity/User.java
+++ src/main/java/com/example/springwithsecurity/entity/User.java
@@ -46,7 +46,7 @@ public class User {
 
     @Column(name = "modified_at")
     private Timestamp modifiedAt;
-    @ManyToMany (cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @ManyToMany (fetch = FetchType.EAGER)
     @JoinTable (name = "users_roles",
             joinColumns = @JoinColumn (name = "user_id"),
             inverseJoinColumns = @JoinColumn (name = "role_id"))
